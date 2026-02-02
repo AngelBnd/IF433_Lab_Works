0ee27b94eea31983a245a948b58126cfef24d449
AngelBnd <angelabenedictins@gmail.com>
2026-02-02 16:34:14 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107781_AngelaBenedictinSunny/week01/GameStore.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week01/GameStore.kt
@@ -3,5 +3,6 @@ package oop_00000107781_AngelaBenedictinSunny.week01
 fun main() {
     val gameTitle: String
     val price: Int
-
 }
+
+fun calculateDiscount(price: Int) : Int = if(price > 500000) (price - (price * 0.2)).toInt() else (price - (price * 0.1)).toInt()
