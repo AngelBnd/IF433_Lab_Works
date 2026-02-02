caeb77a2ea572a0da584aa38c6aff3b80f83aeee
AngelBnd <angelabenedictins@gmail.com>
2026-02-02 15:31:36 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week01/GradingSystem.kt
@@ -0,0 +1,11 @@
+package oop_00000107781_AngelaBenedictinSunny.week01
+
+class GradingSystem {
+}
+
+fun main(){
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama:  " + name + ", Nilai: " + score)
+}
