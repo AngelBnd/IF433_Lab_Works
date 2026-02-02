c3134bba9ad2a65d83aa8a2a6e91f1a51fe794d1
AngelBnd <angelabenedictins@gmail.com>
2026-02-02 16:38:55 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000107781_AngelaBenedictinSunny/week01/GameStore.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week01/GameStore.kt
@@ -3,6 +3,14 @@ package oop_00000107781_AngelaBenedictinSunny.week01
 fun main() {
     val gameTitle: String
     val price: Int
+
+    // initial price value
+    price = 20000
+
+    printReceipt(title = "Test", finalPrice = calculateDiscount(price))
 }
 
 fun calculateDiscount(price: Int) : Int = if(price > 500000) (price - (price * 0.2)).toInt() else (price - (price * 0.1)).toInt()
+fun printReceipt(title: String, finalPrice: Int){
+    println("RECEIPT : Judul $title dan harga final $finalPrice")
+}
