34a597ae3ed6a08d04880738e0fde3f6bf3d6153
AngelBnd <angelabenedictins@gmail.com>
2026-02-09 15:11:51 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week02/Main.kt
@@ -0,0 +1,28 @@
+package oop_00000107781_AngelaBenedictinSunny.week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter ):")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // Program berhenti di sini untuk mahasiswa inim tidak membuat objek
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Instansiasi objek karena data sudah aman
+        val sl = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai")
+    }
+}
