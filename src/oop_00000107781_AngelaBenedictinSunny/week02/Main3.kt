755c9402373b7b32936c4d02012a8dd3200273a5
AngelBnd <angelabenedictins@gmail.com>
2026-02-09 16:20:44 +0700

    week02: (task 2) implement the game

--- src/oop_00000107781_AngelaBenedictinSunny/week02/Main3.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week02/Main3.kt
@@ -1,5 +1,46 @@
 package oop_00000107781_AngelaBenedictinSunny.week02
 import java.util.Scanner
-fun Main3() {
+fun main() {
+    var enemyHp = 100
+    val scanner = Scanner(System.`in`)
+    var menuInput = 0;
+
+    println("halo selamat datang ke gamenya oop ini")
+    print("Masukkan nama heronya :")
+    val namaHeroInput = scanner.nextLine()
+
+    print("Masukkan stat Damageny brp : ")
+    val damageInput = scanner.nextInt()
+
+    val hero = Hero(namaHeroInput, baseDamage = damageInput)
+
+    while(hero.isALive() && enemyHp > 0){
+        println("1. Serang, 2. Kabur")
+        println("pilih :")
+        menuInput = scanner.nextInt()
+
+        if(menuInput == 1) {
+            hero.attack("the enemy")
+            enemyHp -= hero.baseDamage
+            enemyHp = maxOf(0, enemyHp)
+            println("sisa hp musuh : ${enemyHp} ")
+            if(enemyHp >= 0) {
+                hero.takeDamage((10..20).random())
+                println("sisa hp hero: ${hero.hp}")
+            } else break
+        } else if(menuInput == 2) {
+            println("${hero.name} telah kabur, bye bye!")
+            break;
+        } else {
+            println("pilih yg benar ya, 1 atau 2 aj")
+        }
+    }
+
+    if(menuInput!=2){
+        if(hero.isALive() ) println("yg menang adalah ${hero.name} dgn sisa hp ${hero.hp}")
+        else println("musuh telah menang gg dgn sisa health ${enemyHp}")
+    } else {
+        println("karena ${hero.name} kabur dgn sisa health ${hero.hp}, maka secara implisit dia kalah, yahhh :(")
+    }
 
 }
