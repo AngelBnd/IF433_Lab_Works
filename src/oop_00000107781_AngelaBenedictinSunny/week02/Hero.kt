bc1c0dfca3634cdd35568b0a8be5232b430febad
AngelBnd <angelabenedictins@gmail.com>
2026-02-09 15:51:21 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week02/Hero.kt
@@ -0,0 +1,22 @@
+package oop_00000107781_AngelaBenedictinSunny.week02
+
+class Hero(
+    val name: String,
+    var hp: Int,
+    val baseDamage : Int
+) {
+
+    fun attack(targetName: String){
+        println("$name menebas $targetName")
+    }
+
+    fun takeDamage(damage : Int){
+        this.hp -= damage
+        if(this.hp < 0) this.hp = 0
+    }
+
+    fun isALive() : Boolean{
+        return this.hp > 0
+    }
+
+}
