3bdf41c0bcb4ad3ebd2e751c93a0c5dfa700442f
AngelBnd <angelabenedictins@gmail.com>
2026-02-16 15:02:02 +0700

    week03: add private property

--- src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
@@ -1,6 +1,7 @@
 package oop_00000107781_AngelaBenedictinSunny.week03
 
 class Employee(val name: String){
+    private var performanceRating: Int = 3
     var salary: Int = 0
         set(value){
             if( value < 0){
@@ -10,5 +11,14 @@ class Employee(val name: String){
                 field = value
             }
         }
+
+    fun increasePerformanceRating(){
+        performanceRating++
+        println("Kinerja $name meningkat! Rating : $performanceRating")
+    }
+
+    fun printStatus(){
+        println("Karyawan: $name, Rating : $performanceRating")
+    }
 }
 
