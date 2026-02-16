710652848e664105097d3d2f645086cb0a0a700d
AngelBnd <angelabenedictins@gmail.com>
2026-02-16 14:58:55 +0700

    week03: fix setter using backing field

--- src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
@@ -3,9 +3,12 @@ package oop_00000107781_AngelaBenedictinSunny.week03
 class Employee(val name: String){
     var salary: Int = 0
         set(value){
-            // Ini merupakan kesalahan fatal karena this.salary panggil setternya, jadi recursive.
-            println("Mencoba set gaji ke : $value")
-            this.salary = value
+            if( value < 0){
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
 
--- src/oop_00000107781_AngelaBenedictinSunny/week03/Main.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Main.kt
@@ -2,6 +2,5 @@ package oop_00000107781_AngelaBenedictinSunny.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ----")
     e.salary = 5000
 }
