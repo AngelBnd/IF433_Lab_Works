64960c73577a7cbdfb464280a8a6e3f3a0f49bff
AngelBnd <angelabenedictins@gmail.com>
2026-02-16 14:57:08 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_00000107781_AngelaBenedictinSunny.week03
 
 class Employee(val name: String){
     var salary: Int = 0
+        set(value){
+            // Ini merupakan kesalahan fatal karena this.salary panggil setternya, jadi recursive.
+            println("Mencoba set gaji ke : $value")
+            this.salary = value
+        }
 }
+
