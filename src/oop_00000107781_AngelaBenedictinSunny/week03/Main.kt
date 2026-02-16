13838fbd5c9a11fef8e675c9397fa7372ea513ff
AngelBnd <angelabenedictins@gmail.com>
2026-02-16 15:06:54 +0700

    week03: cleanup code

--- src/oop_00000107781_AngelaBenedictinSunny/week03/Main.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Main.kt
@@ -2,16 +2,10 @@ package oop_00000107781_AngelaBenedictinSunny.week03
 
 fun main(){
     val e = Employee("Budi")
-
-    // 1. Test Validasi Salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji :${e.salary}")
 
-    //2. Test Encapsulation
     e.increasePerformance()
-//    e.performanceRating = 5
-
-    //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
