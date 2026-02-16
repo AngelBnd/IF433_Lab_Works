95878d939f116481b5fb776dfd504f033a86a375
AngelBnd <angelabenedictins@gmail.com>
2026-02-16 15:28:35 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week03/Main2.kt
@@ -0,0 +1,11 @@
+package oop_00000107781_AngelaBenedictinSunny.week03
+
+fun main(){
+    val weaponYes = Weapon();
+    weaponYes.damage = -50
+    println(weaponYes.damage)
+    weaponYes.damage = 9999
+    println(weaponYes.damage)
+    println(weaponYes.tier)
+
+}
