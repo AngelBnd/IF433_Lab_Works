7b75b0c5f3d627e5f1dd80f4ce610589709947d9
AngelBnd <angelabenedictins@gmail.com>
2026-03-02 15:40:15 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week05/EWallet.kt
@@ -0,0 +1,16 @@
+package oop_00000107781_AngelaBenedictinSunny.week05
+
+class EWallet(accountName : String, var balance : Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount : Double) {
+        if (balance < amount) {
+            println("Saldo tidak cukup")
+        } else {
+            balance -= amount
+            println("Saldo cukup")
+        }
+    }
+
+    fun topUp(amount : Double) {
+        balance += amount
+    }
+}
