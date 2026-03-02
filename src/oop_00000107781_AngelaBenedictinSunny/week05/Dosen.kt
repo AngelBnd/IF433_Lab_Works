ba0da6d70cc4b278d785baab5e377097593006df
AngelBnd <angelabenedictins@gmail.com>
2026-03-02 13:41:18 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week05/Dosen.kt
@@ -0,0 +1,11 @@
+package oop_00000107781_AngelaBenedictinSunny.week05
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+    override fun bekerja(){
+        println("{$nama} sedang menyiapkan materi perkuliahan dan mereivisi RKPKS. ")
+    }
+
+    fun mengajar(){
+        println("{${nama} sedang mengajar mahasiswa di kelas.")
+    }
+}
