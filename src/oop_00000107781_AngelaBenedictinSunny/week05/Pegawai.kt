25a6edd7da5d6dd7c3267f9c7b51bd96a622d910
AngelBnd <angelabenedictins@gmail.com>
2026-03-02 13:38:22 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000107781_AngelaBenedictinSunny.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak punya body, tidak ada kurung kurawalnya
+    abstract fun bekerja()
+}
