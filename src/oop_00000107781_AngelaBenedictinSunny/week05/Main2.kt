3064fe74f63c343ce96e0cba00ec43ac4f579103
AngelBnd <angelabenedictins@gmail.com>
2026-03-02 15:28:08 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000107781_AngelaBenedictinSunny/week05/Main2.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week05/Main2.kt
@@ -3,6 +3,7 @@ package oop_00000107781_AngelaBenedictinSunny.week05
 fun main() {
     val mathy = MathHelper();
 
+    // salah commit message tadi
     println(mathy.hitungLuas(2))
     println(mathy.hitungLuas(2, 3))
     println(mathy.hitungLuas(jariJari = 2.0))
