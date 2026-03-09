7cd9ec3b9288ee8cc82fd398c0685749f7ca3612
AngelBnd <angelabenedictins@gmail.com>
2026-03-09 14:14:41 +0700

    week06: create Button class implementing Clickable

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week06/Button.kt
@@ -0,0 +1,9 @@
+package oop_00000107781_AngelaBenedictinSunny.week06
+
+// override value bisa di primary constructor
+// class button implementasi interface clickable
+class Button(override val name : String) : Clickable {
+    override fun click(){
+        println("Tombol '$name' berhaisl diklik!" )
+    }
+}
