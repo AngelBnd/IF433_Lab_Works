850f7600171aab60e6fef25b4f95871c58a8c7c9
AngelBnd <angelabenedictins@gmail.com>
2026-03-09 14:12:29 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000107781_AngelaBenedictinSunny/week06/Clickable.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week06/Clickable.kt
@@ -3,7 +3,8 @@ package oop_00000107781_AngelaBenedictinSunny.week06
 interface Clickable {
 
     //Property di dlm interface tidak boleh pny backing dield
-    val name : String = "Tombol Rahasia"
+    // utk memperbaiki, jgn dikasih nilai default yg hrs disimpan
+    val name : String
 
     //function without a body, implicitly abstract
     fun click()
