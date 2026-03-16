0b44b6c12aa0ccf823857a7b143f6e194771bc43
AngelBnd <angelabenedictins@gmail.com>
2026-03-16 12:46:16 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week07/Main.kt
@@ -0,0 +1,6 @@
+package oop_00000107781_AngelaBenedictinSunny.week07
+
+fun main() {
+    // ada garis merah di bawah NetworkClient, karena sifatnya private constructor
+    val client = NetworkClient("https://api.umn.ac.id");
+}
