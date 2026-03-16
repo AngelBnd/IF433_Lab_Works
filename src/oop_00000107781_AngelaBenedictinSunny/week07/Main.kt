584075c1b3e2034e325e4dc595ddcd5a009eccab
AngelBnd <angelabenedictins@gmail.com>
2026-03-16 13:02:40 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000107781_AngelaBenedictinSunny/week07/Main.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week07/Main.kt
@@ -1,6 +1,11 @@
 package oop_00000107781_AngelaBenedictinSunny.week07
 
 fun main() {
-    // ada garis merah di bawah NetworkClient, karena sifatnya private constructor
-    val client = NetworkClient("https://api.umn.ac.id");
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
