fbf6db504e077072f19024593da241b291da6684
AngelBnd <angelabenedictins@gmail.com>
2026-03-16 13:41:37 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000107781_AngelaBenedictinSunny/week07/Main.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week07/Main.kt
@@ -29,5 +29,6 @@ fun main() {
     val uiMessage = when(response){
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Tampilkan: ${response.message}"
+        is ApiResponse.Loading -> "Tampilkan Spinner"
     }
 }
