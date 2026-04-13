dfe7b893c41be96271cf6fd362be99bf22e823cf
AngelBnd <angelabenedictins@gmail.com>
2026-04-13 14:10:37 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000107781_AngelaBenedictinSunny/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000107781_AngelaBenedictinSunny.week08;
+
+public class LegacyJavaAPI {
+    //kotlin gatau hasil dikembalikan null atau engga karena diangagap sebagai playform type
+    public static String fetchServerStatus(){
+        return "SERVER_ONLINE_200_OK";
+    }
+}
