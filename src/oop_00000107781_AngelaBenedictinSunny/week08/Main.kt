592513c18d0efbe939979b9951df3df5e5c56dcc
AngelBnd <angelabenedictins@gmail.com>
2026-04-13 13:55:41 +0700

    week08: implement safe call with let block

--- src/oop_00000107781_AngelaBenedictinSunny/week08/Main.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week08/Main.kt
@@ -6,4 +6,14 @@ fun main() {
 
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "kota tidak diketahui"
     println("tujuan pengiriman: $destination")
+
+    println("test let block")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPridce ?. let {
+        price->
+        val tax = price * 0.11
+        "Transaksi Valid. HArga: Rp $price, Pajak Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di set!"
+    println(receipt)
 }
