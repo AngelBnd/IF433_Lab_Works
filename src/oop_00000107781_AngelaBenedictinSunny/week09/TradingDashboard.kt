a96ed70619b795e0471bb392639cc67a2350f8a6
AngelBnd <angelabenedictins@gmail.com>
2026-04-20 15:19:09 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000107781_AngelaBenedictinSunny/week09/TradingDashboard.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week09/TradingDashboard.kt
@@ -15,6 +15,9 @@ fun main(){
     val topPerformersString = winningTrades.sortedByDescending { it.roe }
         .map{"WIN [${it.pair} - ${it.position}] : +${it.roe}% ROE (Lev: ${it.leverage}x)"}
 
+    val worstPerformerString = losingTrades.sortedByDescending { it.roe }
+        .map{"LOSS [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)"}
+
 
 
 }
