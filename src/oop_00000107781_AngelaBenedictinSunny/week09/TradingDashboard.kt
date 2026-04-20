e44e0255e877c94784e06386b3d0538ce6992e44
AngelBnd <angelabenedictins@gmail.com>
2026-04-20 15:22:18 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000107781_AngelaBenedictinSunny/week09/TradingDashboard.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week09/TradingDashboard.kt
@@ -18,6 +18,12 @@ fun main(){
     val worstPerformerString = losingTrades.sortedByDescending { it.roe }
         .map{"LOSS [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)"}
 
+    println(topPerformersString)
+    println(worstPerformerString)
+
+    val uniquePairs = tradeHistory.map{it.pair}
+        .toSet()
+
 
 
 }
