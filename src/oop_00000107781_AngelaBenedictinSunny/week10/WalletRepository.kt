469033fc217fd8afb8bea4509fb92c5aba650180
AngelBnd <angelabenedictins@gmail.com>
2026-04-27 14:41:52 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000107781_AngelaBenedictinSunny/week10/WalletRepository.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week10/WalletRepository.kt
@@ -3,6 +3,25 @@ package oop_00000107781_AngelaBenedictinSunny.week10
 class WalletRepository<T> {
     private val items = mutableListOf<T>();
 
-    fun add(item : T){}
+    fun add(item : T){
+        items.add(item)
+    }
     fun getAll() : List<T>{return items}
+    fun <T : Any> search(wantedItem : T) : List<Coin>? {
+        val coinValid = wantedItem as? Coin
+        if(coinValid == null){
+            return null
+        } else {
+            val foundItems = mutableListOf<Coin>()
+            for(item in items){
+                var(name, balance) = item as Coin
+                if(name == coinValid.name){
+                    foundItems.add(item)
+                }
+            }
+            return foundItems;
+        }
+
+
+    }
 }
