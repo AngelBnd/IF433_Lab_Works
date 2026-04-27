908417e551eb8b2e7a6149b70de03281bdfc378a
AngelBnd <angelabenedictins@gmail.com>
2026-04-27 14:46:20 +0700

    week10: (task) populate coin repository

--- src/oop_00000107781_AngelaBenedictinSunny/week10/CryptoDashboard.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week10/CryptoDashboard.kt
@@ -3,4 +3,7 @@ package oop_00000107781_AngelaBenedictinSunny.week10
 fun main(){
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("ETH", 24.0))
+    coinRepo.add(Coin("BTC", 22.0))
+    coinRepo.add(Coin("USDT", 20.0))
 }
