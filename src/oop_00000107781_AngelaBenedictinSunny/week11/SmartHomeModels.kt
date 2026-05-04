5ae355094daa9d019c0f2eb3c318aafcad99ac65
AngelBnd <angelabenedictins@gmail.com>
2026-05-04 15:09:58 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000107781_AngelaBenedictinSunny/week11/SmartHomeModels.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week11/SmartHomeModels.kt
@@ -1,3 +1,6 @@
 package oop_00000107781_AngelaBenedictinSunny.week11
 
 data class SmartDevice(var name: String, val category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+fun SmartDevice.diagnose(): String{
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
