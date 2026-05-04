ce85078f7d21a7d976270f6d9773461768d4accb
AngelBnd <angelabenedictins@gmail.com>
2026-05-04 15:15:04 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000107781_AngelaBenedictinSunny/week11/SmartHomeDashboard.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week11/SmartHomeDashboard.kt
@@ -2,4 +2,10 @@ package oop_00000107781_AngelaBenedictinSunny.week11
 
 fun main(){
     val homeDevices = mutableListOf<SmartDevice>()
+
+    homeDevices.apply{
+        add(SmartDevice("Lampu Pintar", "Lighting", true, 12))
+    }.also{
+        println("Added ${it}");
+    }
 }
