cf2b08a2e82f57802bee6152a35ce476b057c35f
AngelBnd <angelabenedictins@gmail.com>
2026-05-11 19:31:37 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000107781_AngelaBenedictinSunny/week12/SmartFeeder.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week12/SmartFeeder.kt
@@ -32,7 +32,7 @@ fun main(){
         println("Siklus pengecekan dispenser pagi selesai")
     }
 
-    runCatching{ dispenseKibble(requestedGram = 30,
+   val result = runCatching{ dispenseKibble(requestedGram = 30,
         availableGram = 1000, isJammed = false) }
         .onSuccess {
             newStock -> currentKibbleStock = newStock
