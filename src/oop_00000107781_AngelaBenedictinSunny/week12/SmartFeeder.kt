2483433a673e4211d0a382f07b8b1c92b68ecf81
AngelBnd <angelabenedictins@gmail.com>
2026-05-11 19:18:56 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000107781_AngelaBenedictinSunny/week12/SmartFeeder.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week12/SmartFeeder.kt
@@ -1,9 +1,15 @@
 package oop_00000107781_AngelaBenedictinSunny.week12
 
 fun dispenseKibble(requestedGram:Int, availableGram:Int, isJammed: Boolean): Int {
+    if(isJammed){
+        throw DispenserJamException();
+    }
+
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
+
+
 
     return availableGram - requestedGram
 
