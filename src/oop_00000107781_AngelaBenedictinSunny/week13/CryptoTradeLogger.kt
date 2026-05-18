45d7ce90a8c2eea1f0b060da02966ef4365acdf2
AngelBnd <angelabenedictins@gmail.com>
2026-05-18 14:07:53 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_00000107781_AngelaBenedictinSunny/week13/CryptoTradeLogger.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week13/CryptoTradeLogger.kt
@@ -3,3 +3,11 @@ package oop_00000107781_AngelaBenedictinSunny.week13
 data class TradeRecord(val id: Int, val symbol: String, val type : String, val margin : Double, val pnl : Double)
 
 fun TradeRecord.toCsv() : String = "$id,$symbol,$type,$margin,$pnl"
+
+fun fromCsvTrade(line: String) : TradeRecord? {
+    val splitted = line.split(",")
+
+    val returnedObj = TradeRecord(splitted[0].toInt(), splitted[1], splitted[2], splitted[3].toDouble(), splitted[4].toDouble())
+
+    if(returnedObj != null){ return returnedObj } else return null
+}
