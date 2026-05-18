91faf68ada40b0338cbbeab1c17d25a74cc1980f
AngelBnd <angelabenedictins@gmail.com>
2026-05-18 14:38:51 +0700

    week13: (task) wrap fromCsvTrade in try-catch for NumberFormatException

--- src/oop_00000107781_AngelaBenedictinSunny/week13/CryptoTradeLogger.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week13/CryptoTradeLogger.kt
@@ -5,9 +5,16 @@ data class TradeRecord(val id: Int, val symbol: String, val type : String, val m
 fun TradeRecord.toCsv() : String = "$id,$symbol,$type,$margin,$pnl"
 
 fun fromCsvTrade(line: String) : TradeRecord? {
-    val splitted = line.split(",")
 
-    val returnedObj = TradeRecord(splitted[0].toInt(), splitted[1], splitted[2], splitted[3].toDouble(), splitted[4].toDouble())
+//    val returnedObj = TradeRecord(splitted[0].toInt(), splitted[1], splitted[2], splitted[3].toDouble(), splitted[4].toDouble())
+//
+//    if(returnedObj != null){ return returnedObj } else return null
+    try {
+        val splitted = line.split(",")
+        return TradeRecord(splitted[0].toInt(), splitted[1], splitted[2], splitted[3].toDouble(), splitted[4].toDouble())
+    } catch ( e : Exception ) {
+        println("(Log) Data Korup diabaikan: $line");
+        return null;
+    }
 
-    if(returnedObj != null){ return returnedObj } else return null
 }
