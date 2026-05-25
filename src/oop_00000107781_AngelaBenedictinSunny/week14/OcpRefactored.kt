0e04bec150f674f4722fe373493020a23fcb1cbb
AngelBnd <angelabenedictins@gmail.com>
2026-05-25 13:24:04 +0700

    week14: refactor DiscountCalculator to use strategy

--- src/oop_00000107781_AngelaBenedictinSunny/week14/OcpRefactored.kt
+++ src/oop_00000107781_AngelaBenedictinSunny/week14/OcpRefactored.kt
@@ -1,13 +1,17 @@
 package oop_00000107781_AngelaBenedictinSunny.week14
 
 interface DiscountStrategy{
-    fun appy(price : Double) : Double
+    fun apply(price : Double) : Double
 }
 
 class StudentDiscount : DiscountStrategy{
-    override fun appy(price : Double) : Double = price * 0.80
+    override fun apply(price : Double) : Double = price * 0.80
 }
 
 class MemberDiscount : DiscountStrategy{
-    override fun appy(price : Double) : Double = price * 0.85
+    override fun apply(price : Double) : Double = price * 0.85
+}
+
+class SafeDiscountCalculator(private val strategy : DiscountStrategy){
+    fun calculate(price : Double) = strategy.apply(price)
 }
